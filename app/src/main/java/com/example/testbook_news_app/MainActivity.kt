dba4e70a17d5509e7bb4189f8c99809494e1e94f
megha-tb <megha.agarwal@testbook.com>
2023-02-19 18:03:17 +0530

    initial commit

--- app/src/main/java/com/example/testbook_news_app/MainActivity.kt
+++ app/src/main/java/com/example/testbook_news_app/MainActivity.kt
@@ -2,20 +2,27 @@ package com.example.testbook_news_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.testbook_news_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
-        replacefragment()
+        replaceFragment(MainFragment())
     }
 
-    private fun replacefragment() {
+    private fun replaceFragment(fragment : Fragment) {
 
-        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, MainFragment.newInstance()).commitNow()
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainerView, fragment)
+        fragmentTransaction.commitNow()
 
     }
-
 
 }
